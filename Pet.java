b8b2980395f4a3c19f0853734b6d93ffc175cc8c
LowryReed <lowry_reed@students.Columbusstate.edu>
2026-02-16 16:06:22 -0500

    Add files via upload

--- Pet.java
+++ Pet.java
@@ -4,27 +4,64 @@
  * This program defines a Pet class with methods to set and get the pet's name,
  * and includes a main method to demonstrate its functionality.
  * 02-08-2026
+ * Updated 02-16-2026
  */
 public class Pet {
     private String name;
+    private String type;
+    private int age;
+
     public Pet() {
         this.name = "";
+        this.type = "Animal";
+        this.age = 1;
     }
+
+    public Pet(String name, String type, int age) {
+        setName(name);
+        setType(type);
+        setAge(age);
+    }
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return this.name;
     }
-    public String toString() {
-        return "Pet information:\nName: " + this.name;
+
+    public String getType() {
+        return this.type;
     }
-    public static void main(String[] args) {
-        Pet pet1 = new Pet();
-        pet1.setName("Guinness");
-        System.out.println(pet1);
-        Pet pet2 = new Pet();
-        pet2.setName("Loki");
-        System.out.println(pet2);
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String speak() {
+        if (type.equals("Dog")) {
+            return "Woof";
+        } else if (type.equals("Cat")) {
+            return "Meow";
+        } else {
+            return "Yowl";
+        }
+    }
+
+    public String toString() {
+        return "Pet information:\n"
+                + "Type: " + type + "\n"
+                + "Name: " + name + "\n"
+                + "Sound: " + speak() + "\n"
+                + "Age:  " + age;
     }
 }
